c9e49735b8ec9b89c48bc6d135c08f1c9e50ef8e
noha-vgf <nohat9177@gmail.com>
2025-12-10 09:51:25 +0100

    Refactor main function for improved parameter handling

--- src/main.c
+++ src/main.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./../include/calculator.h"
+#include "calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(argv[1], "car") == 0) {
+int main(int argc, char *argv[])
+{
+    if (argc < 3) {
+        printf("Erreur de parametres\n");
+        return 1;
+    }
+
+    char *op = argv[1];
+
+    // ✅ CAS "car" = 1 seul paramètre
+    if (strcmp(op, "car") == 0) {
+        double a = atof(argv[2]);
+        printf("%g\n", _car(a));
+        return 0;
+    }
+
+    // ⬇️ à partir d’ici : opérations classiques à 2 paramètres
+    if (argc < 4) {
+        printf("Erreur de parametres\n");
+        return 1;
+    }
+
     double a = atof(argv[2]);
-    printf("%f\n", _car(a));
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
+    double b = atof(argv[3]);
+
+    if (strcmp(op, "add") == 0) printf("%g\n", _add(a, b));
+    else if (strcmp(op, "sub") == 0) printf("%g\n", _sub(a, b));
+    else if (strcmp(op, "mul") == 0) printf("%g\n", _mul(a, b));
+    else if (strcmp(op, "div") == 0) printf("%g\n", _div(a, b));
+    else {
+        printf("Erreur de parametres\n");
+        return 1;
+    }
+
+    return 0;
 }
